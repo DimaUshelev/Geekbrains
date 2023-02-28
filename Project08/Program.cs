9a37aee931f11143c2f56d1ba6a3db6ecefba555
Dmitry <dimaushelev@mail.ru>
2023-02-28 04:26:25 +0300

    Добавил несколько проектов

--- Project08/Program.cs
+++ Project08/Program.cs
@@ -3,15 +3,15 @@ string number = Console.ReadLine();
 
 void CheckingNumber(string number)
 {
-  if (number[0]==number[4] || number[1]==number[3])
-  {
-    Console.WriteLine($"число: {number} - палиндром.");
-  }
-  else Console.WriteLine($"число: {number} - не палиндром.");
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"число: {number} - палиндром.");
+    }
+    else Console.WriteLine($"число: {number} - не палиндром.");
 }
 
 if (number!.Length == 5)
 {
-  CheckingNumber(number);
+    CheckingNumber(number);
 }
 else Console.WriteLine($"Введи правильное число");
