6b357f86ff93f52d604cc261d96d36e00b1ce7b3
Dmitry <dimaushelev@mail.ru>
2023-03-03 05:02:23 +0300

    Добавил 34, 36, 38 задачи

---
+++ Project014/Program.cs
@@ -0,0 +1,71 @@
+﻿int size = 0;
+int[] numbers;
+
+int GetNumber(string text)
+{
+    int num = 0;
+    System.Console.WriteLine(text);
+    num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+string Print(int[] array)
+{
+  string result = String.Empty;
+  int l = array.Length;
+  int index = 0;
+  while (index < l)
+  {
+    result = result + array[index] + "   ";
+    index++;
+  }
+  return result;
+}
+
+int [] FillByRandom(int size)
+{
+
+int [] array = new int [size];
+int min=100;
+int max=999;
+int first = 0;
+
+for (int index=0; index < array.Length; index++)
+{
+
+first = new Random().Next(min,max);
+
+array[index] = first;
+
+}
+
+return array;
+} 
+
+int ChetNumbers(int [] array)
+{
+  string result = String.Empty;
+int cifra = 0;
+
+for (int index=0; index < array.Length; index++)
+{
+    if ((array[index] % 2) == 0)
+    {
+        cifra = cifra + 1;
+        result = result + array[index] + "   ";
+    }
+
+}
+Console.WriteLine($"Четные элементы массива:   {result}");  
+return cifra;
+} 
+
+size = GetNumber("Введите размер массива ");
+
+numbers = FillByRandom(size); 
+
+Console.WriteLine("Массив");  
+
+Console.WriteLine(Print(numbers));
+
+Console.WriteLine($"Количество четных элементов массива = {ChetNumbers(numbers)}");
---
+++ Project016/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int n = 0; n < numbers.Length; n++)
+{
+    if (numbers[n] > max)
+        {
+            max = numbers[n];
+        }
+    if (numbers[n] < min)
+        {
+            min = numbers[n];
+        }
+}
+
+Console.WriteLine($"всего {numbers.Length} чисел. Макс значение= {max}, мин значение = {min}");
+Console.WriteLine($"Разница между макс и мин значениями = {max - min}");
+
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
